aa64786fde7a4a53f317062ff6abcb575e4a6731
Chessboy71 <48567451+Chessboy71@users.noreply.github.com>
2020-05-10 03:19:28 +0200

    first try

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+const port = 3000;
 // Require Express to run server and routes
-
+const express = require('express'); 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
 // Initialize the main project folder
 app.use(express.static('website'));
-
-
 // Setup Server
+app.get('/', (req, res) => {
+    res.send(projectData);
+    console.log('heey');
+})
+app.post('/', function(req,res){
+    let newData = res.body; 
+    projectData = newData;
+})
+
+app.listen(port, () => {
+    console.log(`LocalHost : ${port}`)
+})
+
--- website/app.js
+++ website/app.js
@@ -3,3 +3,22 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const postData = async (url='',data = {}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method:'POST',
+        credentials:'same-origin',
+        headers: {
+            'Content-Type': 'application.json'
+        },
+        body: JSON.stringify(data)
+    });
+    try{
+        const newData = await response.json();
+        console.log(newData)
+    }catch(error){
+        console.log('error', error);
+    }
+}
+postData('/', {movie:'helloWorld'})
